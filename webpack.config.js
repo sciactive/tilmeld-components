4d4530eb533bda2fd28a3a55baffe7fa0a7d9c69
Hunter Perrin <hperrin@gmail.com>
2019-05-01 18:34:11 -0700

    Fix wrong library epxort.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    library: ['tilmeld-client'],
+    library: ['tilmeld-components'],
     libraryTarget: 'umd',
     globalObject: 'this',
   },
